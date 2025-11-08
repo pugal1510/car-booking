a2bb508577bf7690c899882e135b20750e523ae1
pugal <pugalthangavel10@gmail.com>
2025-11-08 22:27:32 +0530

    updating the release branch

---
+++ Car_Booking/src/test/java/com/example/Car_Booking/CarBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Car_Booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
